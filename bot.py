e5e7e59c60bfd5c15b398cca8f17da0346b44687
dwong28 <dwong28@calpoly.edu>
2017-01-04 00:44:29 -0800

    made some changes

--- bot.py
+++ bot.py
@@ -1,40 +1,57 @@
 import discord
+import leagueCommands
 from botInfo import token
 from discord.ext import commands
 import asyncio
+description = "test bot"
+#client = discord.Client()
+commandBot = commands.Bot(command_prefix='!', description=description)
 
-client = discord.Client()
-
-
-@client.event
+@commandBot.event
 @asyncio.coroutine
 def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(commandBot.user.name)
+    print(commandBot.user.id)
     print('------')
 
 
-@client.event
+#@client.event
+#@asyncio.coroutine
+#def on_message(message):
+#    if message.content.startswith('l!serverStats'):
+#        leagueCommands.server_status(message.content[1])
+#        print(message.content[1])
+#    elif message.content.startswith('!add'):
+#        add(message.content[1], message.content[2])
+#    elif message.content.startswith('!sleep'):
+#        yield from client.send_message(message.channel, "Sleeping for 5 secs. Oyasumi~~")
+#        yield from asyncio.sleep(5)
+#        yield from client.send_message(message.channel, 'Done sleeping')
+#    elif message.content.startswith('!eat'):
+#        yield from client.send_message(message.channel, "Time to eat! NOM NOM NOM")
+#    elif message.content.startswith('!adrian'):
+#        yield from client.send_message(message.channel, ':adriangasm:')
+
+@commandBot.command()
 @asyncio.coroutine
-def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = yield from client.send_message(message.channel, 'Calculating messages...')
-        asyncio.ensure_future()
-        for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+def add( left : int, right : int):
+    """Adds two numbers together."""
+    print ("adding")
+    yield from commandBot.say((left + right))
 
-        yield from client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        yield from client.send_message(message.channel, "Sleeping for 5 secs. Oyasumi~~")
-        yield from asyncio.sleep(5)
-        yield from client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('!eat'):
-        yield from client.send_message(message.channel, "Time to eat! NOM NOM NOM")
-    elif message.content.startswith('!adrian'):
-        yield from client.send_message(message.channel, ':adriangasm:')
 
-client.run(token)
+@commandBot.command()
+@asyncio.coroutine
+def eat():
+    """Eat"""
+    yield from commandBot.say("Time to eat! NOM NOM NOM")
+
+@commandBot.command()
+@asyncio.coroutine
+def joined(member : discord.Member):
+    """Says when a member joined."""
+    yield from commandBot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+commandBot.run(token)
 
